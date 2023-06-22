7fcecc3815c316bab879636d34f4383cbd7db8d8
Pablo Correa <pejotace22@gmail.com>
2023-06-22 00:45:12 -0300

    parcial resuleto

--- Parcial/parcial.ino
+++ Parcial/parcial.ino
@@ -1,4 +1,6 @@
-#include <Wire.h>
+/* En thinkercad esta el circuito:
+https://www.tinkercad.com/things/4UIilOzASQ1-copy-of-parcial-integrador-2023/editel?sharecode=miHhWJGy-nkXlX8eAezv1sGNDaTTRJgwcqFRmCSwF5w*/
+
 #include <LiquidCrystal_I2C.h>
 
 #define A 2
@@ -7,29 +9,24 @@
 #define MOTOR 3
 #define RPV 159
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7);
-
-void encoder();
-int vueltas(int pulsos);
-bool leerBotones();
-void display();
-
-int anterior = 0;
-int giros = 0;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 volatile int conteo = 0;
+int giros = 0, vueltas = 0;
+bool accion = false;
+
 unsigned long tiempoVuelta = 0;
+
+void encoder();
+bool leerBotones();
+void display();
+void resetear();
 
 void setup()
 {
   Serial.begin(9600);
   lcd.init();
-  lcd.backLight();
-  /*
-  lcd.begin(16,2);
-  lcd.setBacklightPin(3,POSITIVE);
-  lcd.setBacklight(HIGH);
-  */
+  lcd.backlight();
   lcd.setCursor(2, 0);
   lcd.print("Grupazo uno");
 
@@ -39,41 +36,37 @@ void setup()
   pinMode(MOTOR, OUTPUT);
 
   delay(2000);
-  attachInterrupt(digitalPinToInterrupt(A), encoder, FALLING);
+  attachInterrupt(digitalPinToInterrupt(A), encoder, RISING);
 }
 
 void loop()
 {
-  static bool accion = false;
   if (!accion)
   {
     accion = leerBotones();
   }
-
+  
+  vueltas = conteo / RPV;
+  
   display();
 
-  // Pregunta si se llego a la cuenta
-  if (vueltas(conteo) >= giros)
+  // Pregunta si se llegó a la cuenta
+  if (vueltas >= giros && giros != 0)
   {
+    Serial.println("Motor stop");
     digitalWrite(MOTOR, LOW);
     accion = false;
     giros = 0;
+    while(!accion)
+    {
+      accion = leerBotones();
+    }
   }
 }
 
 void encoder()
 {
-  static unsigned long tiempoFiltro = 0;
-  if (millis() - tiempoFiltro >= 5)
-  {
-    conteo++;
-    tiempoFiltro = millis();
-  }
-}
-
-int vueltas(int pulsos)
-{
-  return pulsos / RPV;
+  conteo++;
 }
 
 bool leerBotones()
@@ -81,18 +74,14 @@ bool leerBotones()
   if (digitalRead(BOT10))
   {
     giros = 10;
-    conteo = 0;
-    digitalWrite(MOTOR, HIGH);
-    tiempoVuelta = millis();
+    resetear();
     return true;
   }
 
   if (digitalRead(BOT100))
   {
     giros = 100;
-    conteo = 0;
-    digitalWrite(MOTOR, HIGH);
-    tiempoVuelta = millis();
+    resetear();
     return true;
   }
 
@@ -101,22 +90,47 @@ bool leerBotones()
 
 void display()
 {
-  static unsigned long muestreo = 0;
+  static int muestraAnterior = -1;
+  static int vueltaAnterior = -1;
+  static bool accionAnterior = true;
+  
+  int vueltasActuales = vueltas;
+  String texto[2] = {"Off", "On "};
 
-  if (millis() - muestreo >= 500)
+  if (conteo != muestraAnterior)
   {
+    muestraAnterior = conteo;
     Serial.println(conteo);
+  }
+
+  if (vueltasActuales != vueltaAnterior)
+  {
+    vueltaAnterior = vueltasActuales;
     lcd.clear();
     lcd.setCursor(2, 0);
     lcd.print("Giros:");
     lcd.setCursor(10, 0);
-    lcd.print(vueltas(conteo));
+    lcd.print(vueltasActuales);
 
-    lcd.setCursor(0, 1);
+    lcd.setCursor(1, 1);
     lcd.print("Tiempo:");
     lcd.setCursor(10, 1);
     lcd.print(millis() - tiempoVuelta);
-
-    muestreo = millis();
   }
+
+  if (accion != accionAnterior)
+  {
+    lcd.setCursor(13, 0);
+    lcd.print(texto[accion]);
+    accionAnterior = accion;
+  }
+}
+
+void resetear()
+{
+  conteo = 0;
+  digitalWrite(MOTOR, HIGH);
+  tiempoVuelta = millis();
+  Serial.println("Valores a cero");
+  Serial.println("Motor on");
 }
